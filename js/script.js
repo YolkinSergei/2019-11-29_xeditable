c03452946b373caf1b5c878ac99e5ba49839c9ad
YolkinSergei <yolkin.sereja@gmail.com>
2019-12-01 00:46:23 +0200

    homework done

--- js/script.js
+++ js/script.js
@@ -13,14 +13,29 @@ function clickHandler(event) {
 	let inputElement = document.createElement('input');
 	inputElement.setAttribute('type', targetType);
 	inputElement.value = targetElement.innerText;
+	inputElement.classList.add('form-control');
+
+	let submitBtn = createButton({
+		icon: 'glyphicon glyphicon-ok',
+		style: 'btn btn-primary btn-xs',
+		handler: function() {
+			targetElement.innerText = inputElement.value;
+		}
+	});
+
+	let cancelBtn = createButton({
+		icon: 'glyphicon glyphicon-remove',
+		style: 'btn btn-danger btn-xs'
+	});
 
 	parentElement.appendChild(inputElement);
 	parentElement.removeChild(targetElement);
+	parentElement.appendChild(submitBtn);
+	parentElement.appendChild(cancelBtn);
 
 	inputElement.select();
 
 	inputElement.addEventListener('keyup', function(event) {
-		console.log(event);
 		switch(event.which) {
 			case 13: {
 				targetElement.innerText = inputElement.value;
@@ -34,8 +49,38 @@ function clickHandler(event) {
 		}
 	});
 
-	inputElement.addEventListener('blur', function(argument) {
+	inputElement.addEventListener('blur', function(event) {
 		parentElement.appendChild(targetElement);
 		parentElement.removeChild(inputElement);
+		parentElement.removeChild(submitBtn);
+		parentElement.removeChild(cancelBtn);
 	});
 }
+
+function createButton(config) {
+	let btn = document.createElement('button');
+
+	if (config.style) btn.classList.add(...config.style.split(' '));
+
+	if (config.icon) {
+		let span = document.createElement('span');
+		span.classList.add(...config.icon.split(' '));
+		btn.appendChild(span);
+	}
+
+	if (config.handler) btn.addEventListener('mousedown', config.handler);
+
+	return btn;
+}
+
+/*
+ET: 3 часа 
+
+АТ: 2 часа 37 минут
+
+note: Прошло 2 часа и кнопки по-прежнему не работают из-за того, что в слушателе 
+	blur-a я их удаляю и соответственно слушатель click на кнопках не срабатывает.
+
+note: после написания предыдущей заметки пришла гениальная мысль загуглить порядок
+	event-ов. Надо вешаться не на click, а на mousedown.
+*/ 
